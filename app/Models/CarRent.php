c5a8a4f372e37e9b1e162e8169083e2ce093a97b
Ade Nova <nttnova3@gmail.com>
2024-05-01 17:08:42 +0700

    db-motor db-mobil

---
+++ app/Models/CarRent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarRent extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'car_name',
+        'price',
+        'car_image',
+        'seat',
+        'car_trunk',
+        'transmisi',
+        'fuel',
+    ];
+}
